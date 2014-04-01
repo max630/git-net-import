878c9c0239537f10ce82f3bd54793908af16ee80
Max Kirillov <max@max630.net>
2014-04-01 23:38:39 +0300

    IMPL: some real test

--- git-net-import-test/Main.cs
+++ git-net-import-test/Main.cs
@@ -1,10 +1,17 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using gitnetimport;
 
 namespace gitnetimporttest {
     class MainClass {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var i = new Import();
+
+            using (var w = new BinaryWriter(Console.OpenStandardOutput())) {
+                i.Run(w, "master", new Import.Commit[] {});
+            }
         }
     }
 }
